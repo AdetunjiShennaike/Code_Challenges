2108311aada530eb91d1698053ed5701a996dbd4
Adetunji <Ashennaike@gmail.com>
2019-10-09 00:56:28 -0400

    photo occurences

--- CodeWars.js
+++ CodeWars.js
@@ -6,8 +6,6 @@ Sometimes there are problems: lack of colors, technical malfunction and a "bad" 
 You have to write a function printer_error which given a string will output the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. 
 Don't reduce this fraction to a simpler expression.
 
-The string has a length greater or equal to one and contains only letters from ato z.
-
 #Examples:
 
 s="aaabbbbhaijjjm"
@@ -33,7 +31,7 @@ isIsogram("moOse") == false // -- ignore letter case*/
 
 function isIsogram(str){
   for(let i = 0; i < str.length; i++){
-    for(let j = i+1; j<str.length; j++){
+    for(let j = i+1; j < str.length; j++){
       if(str[i].toLowerCase().includes(str[j].toLowerCase())){
         return false
       }
@@ -45,4 +43,32 @@ function isIsogram(str){
 //optimal code
 function isIsogram(str){
   return new Set(str.toUpperCase()).size == str.length;
+}
+
+
+/*Enough is enough!
+Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like this sessions, since the motive usually repeats. He isn't fond of seeing the Eiffel tower 40 times. He tells them that he will only sit during the session if they show the same motive at most N times. Luckily, Alice and Bob are able to encode the motive as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+
+Task
+Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. 
+For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+
+Example
+  deleteNth ([1,1,1,1],2) // return [1,1]
+
+  deleteNth ([20,37,20,21],1) // return [20,37,21]*/
+
+
+function deleteNth(arr,n){
+  for(let i = 0;i < arr.length; i++){
+  let count = 1
+    for(let j = i+1;j < arr.length; j++){
+      if(arr[i] === arr[j] && count !== n){
+        count++
+      }else if(arr[i] === arr[j] && count >= n){
+        arr.splice(j,1) && j--
+      }
+    }
+  }
+  return arr
 }
