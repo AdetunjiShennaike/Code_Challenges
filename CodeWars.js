f4fb93f3d119df8e516f07c4c6631bf45444db6d
Adetunji <Ashennaike@gmail.com>
2019-10-09 00:19:45 -0400

    isogram challenge

--- CodeWars.js
+++ CodeWars.js
@@ -1,10 +1,10 @@
-/*In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
-
-The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
-
+/*In a factory a printer prints labels for boxes. 
+For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+The colors used by the printer are recorded in a control string. 
+For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
 Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
-
-You have to write a function printer_error which given a string will output the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+You have to write a function printer_error which given a string will output the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. 
+Don't reduce this fraction to a simpler expression.
 
 The string has a length greater or equal to one and contains only letters from ato z.
 
@@ -17,8 +17,32 @@ s="aaaxbbbbyyhwawiwjjjwwm"
 error_printer(s) => "8/22"*/
 
 function printerError(s) {
-    let count = 0
-    let arr = s.toLowerCase().split('')
-    arr.forEach(e => {if(e > 'm'){count++}})
-    return `${count}/${s.length}`
+  let count = 0
+  let arr = s.toLowerCase().split('')
+  arr.forEach(e => {if(e > 'm'){count++}})
+  return `${count}/${s.length}`
+}
+
+/*An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+Implement a function that determines whether a string that contains only letters is an isogram. 
+Assume the empty string is an isogram. Ignore letter case.
+
+isIsogram("Dermatoglyphics") == true
+isIsogram("aba") == false
+isIsogram("moOse") == false // -- ignore letter case*/
+
+function isIsogram(str){
+  for(let i = 0; i < str.length; i++){
+    for(let j = i+1; j<str.length; j++){
+      if(str[i].toLowerCase().includes(str[j].toLowerCase())){
+        return false
+      }
+    }
   }
+  return true
+}
+
+//optimal code
+function isIsogram(str){
+  return new Set(str.toUpperCase()).size == str.length;
+}
