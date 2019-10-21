66520e4afbc0203e1e47434d40e06d9268a4dab7
Adetunji <ashennaike@gmail.com>
2019-10-20 23:41:32 -0400

    codewars

--- CodeWars.js
+++ CodeWars.js
@@ -138,3 +138,38 @@ function titleCase(title, minorWords) {
   }
   return words.join(' ')
 }
+
+//optimal code
+function titleCase(title, minorWords) {
+  var minorWords = typeof minorWords !== "undefined" ? minorWords.toLowerCase().split(' ') : [];
+  return title.toLowerCase().split(' ').map(function(v, i) {
+    if(v != "" && ( (minorWords.indexOf(v) === -1) || i == 0)) {
+      v = v.split('');
+      v[0] = v[0].toUpperCase();
+      v = v.join('');
+    }
+    return v;
+  }).join(' ');
+}
+
+/*This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
+
+function accum(s) {
+	let ans = ''
+  for(let i = 0;i < s.length;i++){
+    ans += s[i].toUpperCase()
+    for(let j = 0;j < i; j++){
+      ans += s[i].toLowerCase()
+    }
+    i != s.length-1 ? ans += '-' : console.log('done')
+  }
+  return ans
+}
