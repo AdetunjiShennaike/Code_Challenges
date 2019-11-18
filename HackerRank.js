246490fbc7305d7744b2e76b2e7b3a0ffb93169d
Adetunji <Ashennaike@gmail.com>
2019-11-17 23:29:10 -0500

    hacker rank

--- HackerRank.js
+++ HackerRank.js
@@ -3,8 +3,16 @@ function compareTriplets(a, b) {
   let countA = 0
   let countB = 0
   for( let i = 0;i < a.length;i++ ){
-      if(a[i]>b[i]) countA++ 
-      if(a[i]<b[i]) countB++ 
+    if(a[i]>b[i]) countA++ 
+    if(a[i]<b[i]) countB++ 
   }
 return [countA, countB]
+}
+
+function aVeryBigSum(ar) {
+  let ans = 0
+  ar.forEach(e => {
+    ans += e
+  })
+  return ans
 }
