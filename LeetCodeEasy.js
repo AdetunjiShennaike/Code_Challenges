96668440011dd26a468d5dcbfbecf8ad40484ae5
Adetunji <Ashennaike@gmail.com>
2020-01-02 00:04:54 -0500

    leetcode

--- LeetCodeEasy.js
+++ LeetCodeEasy.js
@@ -389,3 +389,13 @@ var mySqrt = function(x) {
     }
   }
 };// without math library
+
+
+// Question 70 Climbing Stairs
+var climbStairs = function(n) {
+  if(n == 0){ return 0 }
+  if(n == 1){ return 1 }
+  if(n == 2){ return 2 }
+  
+  return climbStairs(n-1) + climbStairs(n-2)
+};
