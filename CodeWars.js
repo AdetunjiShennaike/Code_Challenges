e841092021a08a04f309073aa0c41e04ce265016
Adetunji <Ashennaike@gmail.com>
2020-02-13 16:50:14 -0500

    codewars

--- CodeWars.js
+++ CodeWars.js
@@ -556,3 +556,25 @@ function order(words){
   return newString.join(' ')
 }
 
+
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+var moveZeros = function (arr){
+  let count = 0
+  for(let i = 0;i < arr.length;i++){
+    if(arr[i] === 0){
+      count++
+      arr.splice(i, 1)
+      i--
+    }
+  }
+  while(count > 0){
+    arr.push(0)
+    count--
+  }
+  return arr
+}
