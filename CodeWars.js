787d56ee7d13aed738cf8886fe79b1e2ad320a62
Adetunji <Ashennaike@gmail.com>
2020-03-01 22:26:29 -0500

    code wars

--- CodeWars.js
+++ CodeWars.js
@@ -701,3 +701,34 @@ function diamond(n){
   return ans;
 }
 
+
+
+/*
+Create a function that accepts dimensions, of Rows x Columns, as parameters in order to create a multiplication table sized according to the given dimensions. **The return value of the function must be an array, and the numbers must be Fixnums, NOT strings.
+
+Example:
+
+multiplication_table(3,3)
+
+1 2 3
+2 4 6
+3 6 9
+
+-->[[1,2,3],[2,4,6],[3,6,9]]
+
+Each value on the table should be equal to the value of multiplying the number in its first row times the number in its first column.
+*/
+
+
+function multiplicationTable(row,col){
+  let ans = []
+  for(let i = 0;i < row;i++){
+    let temp = []
+    for(let j = 0;j < col;j++){
+      let cur = (j+1) * (i+1)
+      temp.push(cur)
+    }
+    ans.push(temp)
+  }
+  return ans
+}
